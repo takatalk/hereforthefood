9877037f291298d1ee863fd5fdb1843f3ad9d6e3
fourstates <taka@noetic.co>
2017-06-24 19:18:16 -0700

    resize test

--- predict.py
+++ predict.py
@@ -1,11 +1,13 @@
 import sys
 from keras.models import model_from_json
 from skimage.novice import open as sci_open
+import numpy as np
 import json
 
 if __name__ == '__main__':
 	filename = sys.argv[1]
 	array = sci_open(filename).array
+	array = np.expand_dims(array, axis=0)
 	test = [array]
 	model_file = open('model.json', 'r').readline()
 	json_model = json.loads(model_file)
