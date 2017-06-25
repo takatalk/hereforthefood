1f9903072a95a0c55b90cea1639932b871b8ea37
fourstates <taka@noetic.co>
2017-06-24 18:41:23 -0700

    predictor

---
+++ predict.py
@@ -0,0 +1,14 @@
+import sys
+from keras.models import model_from_json
+from skimage.novice import open as sci_open
+
+
+if __name__ == '__main__':
+	filename = sys.argv[1]
+	array = sci_open(filename).array
+	test = [array]
+	print(test)
+	json = open('model.json', 'r')
+	model = model_from_json(json)
+	model.load_weights('model_weights.hdf', by_name=False)
+	model.predict(test, batch_size=1, verbose=0)
