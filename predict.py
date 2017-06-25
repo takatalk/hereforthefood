1931569f1faacf738fb7777318e4794cb8ec0364
Lap-Tak Chu <lchu@alumni.ubc.ca>
2017-06-24 19:52:00 -0700

    made it more modular....

--- predict.py
+++ predict.py
@@ -5,15 +5,18 @@ import json
 from keras.preprocessing.image import img_to_array, load_img
 
 if __name__ == '__main__':
-	filename = sys.argv[1]
-	img = load_img(filename)
-	array = img_to_array(img)
-	array.reshape((1,) + array.shape)
-	# array = np.expand_dims(array, axis=0)
-	# array = array[1,:]
-	test = [array]
-	model_file = open('model.json', 'r').readline()
-	json_model = json.loads(model_file)
-	model = model_from_json(json_model)
-	model.load_weights('weights.hdf', by_name=False)
-	model.predict(test, batch_size=1, verbose=0)
+        test_list = []
+        filename = sys.argv[1]
+        img = load_img(filename)
+        array = img_to_array(img)
+        # array = np.expand_dims(array, axis=0)
+        # array = array[1,:]
+        test_list.append(array)
+        test = np.stack(test_list)
+        #test = test.reshape(test.shape[0], 301, 301, 3)
+        model_file = open('model.json', 'r').readline()
+        json_model = json.loads(model_file)
+        model = model_from_json(json_model)
+        model.load_weights('weights.hdf', by_name=False)
+        predictions = model.predict(test, batch_size=1, verbose=0)
+        print(predictions)
