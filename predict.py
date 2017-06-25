4da03f05401f757dddb4370af32f2df06a47feb0
Lap-Tak Chu <lchu@alumni.ubc.ca>
2017-06-24 19:29:47 -0700

    switched out skimage with built in keras image reader

--- predict.py
+++ predict.py
@@ -1,14 +1,15 @@
 import sys
 from keras.models import model_from_json
-from skimage.novice import open as sci_open
 import numpy as np
 import json
+from keras.preprocessing.image import img_to_array, load_img
 
 if __name__ == '__main__':
 	filename = sys.argv[1]
-	array = sci_open(filename).array
+	img = load_img(filename)
+	array = img_to_array(img)
 	# array = np.expand_dims(array, axis=0)
-	array = array[1,:]
+	# array = array[1,:]
 	test = [array]
 	model_file = open('model.json', 'r').readline()
 	json_model = json.loads(model_file)
